b7cf39ad363ebe59a4dd313adbf134a7ba3ba76d
onmybob <bobuuid@gmail.com>
2024-01-19 12:11:22 -0700

    ini

---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Inter, ZCOOL_XiaoWei } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const chinese = ZCOOL_XiaoWei({ weight: ['400'], subsets: ['latin'] });

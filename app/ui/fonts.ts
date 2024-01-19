47acaec5c96a88ace43535b8dc3873a2257e7566
onmybob <bobuuid@gmail.com>
2024-01-19 12:17:17 -0700

    f

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Inter, ZCOOL_XiaoWei } from 'next/font/google';
+import { Inter, ZCOOL_XiaoWei, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 export const chinese = ZCOOL_XiaoWei({ weight: ['400'], subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '400'],
+  subsets: ['latin'],
+});
